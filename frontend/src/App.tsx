c7324533b1eb59370081126bfcd58d7a8f9d18a0
DFernJ <dfernj02@estudiantes.unileon.es>
2026-06-06 04:37:24 +0200

    feat: Component to redirect login and register routes if user has a valid session

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import AuthenticatedLayoutRoute from "./components/AuthenticatedLayoutRoute";
+import PublicOnlyRoute from "./components/PublicOnlyRoute";
 import AdminPage from "./pages/AdminPage";
 import DashboardPage from "./pages/DashboardPage";
 import ExercisePage from "./pages/ExercisePage";
@@ -13,8 +14,10 @@ import SupportPage from "./pages/SupportPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<PublicOnlyRoute />}>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
       <Route element={<AuthenticatedLayoutRoute />}>
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/exercises" element={<ExercisePage />} />
