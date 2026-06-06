ec18a331273aa02378153ed1a4551c1d0b4d62d9
DFernJ <dfernj02@estudiantes.unileon.es>
2026-06-06 18:08:37 +0200

    feat: added legal routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,10 @@ import ProfilePage from "./pages/ProfilePage";
 import RegisterPage from "./pages/RegisterPage";
 import UserRoutinesPage from "./pages/UserRoutinesPage";
 import SupportPage from "./pages/SupportPage";
+import LegalNoticePage from "@/pages/LegalNoticePage.tsx";
+import PrivacyPolicyPage from "@/pages/PrivacyPolicyPage.tsx";
+import CookiesPolicyPage from "@/pages/CookiesPolicyPage.tsx";
+import TermsPage from "@/pages/TermsPage.tsx";
 
 export default function App() {
   return (
@@ -22,11 +26,14 @@ export default function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/exercises" element={<ExercisePage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/routines" element={<UserRoutinesPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/support" element={<SupportPage />} />
-        
+        <Route path="/routines" element={<UserRoutinesPage />} />
       </Route>
+        <Route path="/legal/aviso-legal" element={<LegalNoticePage />}/>
+        <Route path="/legal/privacidad" element={<PrivacyPolicyPage />}/>
+        <Route path="/legal/cookies" element={<CookiesPolicyPage />}/>
+        <Route path="/legal/terminos" element={<TermsPage />}/>
       <Route path="*" element={<NotFoundRedirect />} />
     </Routes>
   );
